c6ed00ff33275d4f99704f04adf3faa8a9815f4f
Robert Carmona <robert.carmona@enspiresoftware.com>
2016-08-30 12:54:39 -0400

    use credentials if present

--- lib/jira/http_client.rb
+++ lib/jira/http_client.rb
@@ -17,7 +17,7 @@ module JIRA
     end
 
     def make_cookie_auth_request
-      unless cookies?
+      if @options[:username].present? && @options[:password].present?
         body = { username: @options[:username],
                  password: @options[:password] }.to_json
       end
@@ -31,7 +31,7 @@ module JIRA
       request = Net::HTTP.const_get(http_method.to_s.capitalize).new(path, headers)
       request.body = body unless body.nil?
       add_cookies(request) if options[:use_cookies]
-      request.basic_auth(@options[:username], @options[:password])
+      request.basic_auth(@options[:username], @options[:password]) if @options[:username].present? && @options[:password].present?
       response = basic_auth_http_conn.request(request)
       store_cookies(response) if options[:use_cookies]
       response
