601cab8e1e767e0417d8167f69e9bab33e48ec85
Robert Carmona <robert.carmona@enspiresoftware.com>
2016-08-30 08:06:42 -0400

    remove go cookies requirement

--- lib/jira/http_client.rb
+++ lib/jira/http_client.rb
@@ -30,13 +30,13 @@ module JIRA
     end
 
     def make_request(http_method, path, body = '', headers = {})
-      headers['cookie'] = "JSESSIONID=#{@cookies['JSESSIONID'].first}" if got_cookies?
+      #headers['cookie'] = "JSESSIONID=#{@cookies['JSESSIONID'].first}" if got_cookies?
       request = Net::HTTP.const_get(http_method.to_s.capitalize).new(path, headers)
       request.body = body unless body.nil?
       add_cookies(request) if options[:use_cookies]
-      request.basic_auth(@options[:username], @options[:password]) unless got_cookies?
+      request.basic_auth(@options[:username], @options[:password]) #unless got_cookies?
       response = basic_auth_http_conn.request(request)
-      store_cookies(response) if options[:use_cookies] && !got_cookies?
+      store_cookies(response) if options[:use_cookies] #&& !got_cookies?
       response
     end
 
